9575ed06284504e384d077db308088c716f80f17
Roman Zagorowski <roman.zagorowski@hotmail.com>
2023-12-23 10:48:47 +0100

    Add project files.

---
+++ ordercache01/OrderCacheImpl01.cpp
@@ -0,0 +1,31 @@
+#include "OrderCacheImpl01.h"
+
+void OrderCacheImpl01::addOrder(Order order)
+{
+    throw "Not implemented yet";
+}
+
+void OrderCacheImpl01::cancelOrder(const std::string& orderId)
+{
+    throw "Not implemented yet";
+}
+
+void OrderCacheImpl01::cancelOrdersForUser(const std::string& user)
+{
+    throw "Not implemented yet";
+}
+
+void OrderCacheImpl01::cancelOrdersForSecIdWithMinimumQty(const std::string& securityId, unsigned int minQty)
+{
+    throw "Not implemented yet";
+}
+
+unsigned int OrderCacheImpl01::getMatchingSizeForSecurity(const std::string& securityId)
+{
+    throw "Not implemented yet";
+}
+
+std::vector<Order> OrderCacheImpl01::getAllOrders() const
+{
+    throw "Not implemented yet";
+}
---
+++ ordercache01/OrderCacheImpl01.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "OrderCacheInterface.h"
+
+class OrderCacheImpl01 : public OrderCacheInterface
+{
+public:
+    // add order to the cache
+    void addOrder(Order order) override;
+
+    // remove order with this unique order id from the cache
+    void cancelOrder(const std::string& orderId) override;
+
+    // remove all orders in the cache for this user
+    void cancelOrdersForUser(const std::string& user) override;
+
+    // remove all orders in the cache for this security with qty >= minQty
+    void cancelOrdersForSecIdWithMinimumQty(const std::string& securityId, unsigned int minQty) override;
+
+    // return the total qty that can match for the security id
+    unsigned int getMatchingSizeForSecurity(const std::string& securityId) override;
+
+    // return all orders in cache in a vector
+    std::vector<Order> getAllOrders() const override;
+
+private:
+
+};
