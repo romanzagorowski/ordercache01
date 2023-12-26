d1ab2a03ef215598a6e8420d3c130f36622c9df0
Roman Zagorowski <roman.zagorowski@hotmail.com>
2023-12-26 10:31:01 +0100

    Comments added

--- ordercache01/OrderCache.cpp
+++ ordercache01/OrderCache.cpp
@@ -2,7 +2,6 @@
 
 #include <algorithm>
 #include <set>
-#include <cassert>
 
 void OrderCache::addOrder(Order order)
 {
@@ -25,6 +24,7 @@ void OrderCache::cancelOrder(const std::string& orderId)
         }
     };
 
+    // An iterator to the order is needed to remove it form the indexes.
     const auto it_order = orders_table.find(IdOnlyOrder{ orderId });
 
     if(it_order != orders_table.end())
@@ -39,23 +39,29 @@ void OrderCache::cancelOrder(const std::string& orderId)
 
 void OrderCache::cancelOrdersForUser(const std::string& user)
 {
+    // Get a range with all orders for the user.
     auto [range_begin, range_end] = user_index.equal_range(user);
 
+    // For every order in range...
     for(auto& it = range_begin; it != range_end; ++it)
     {
         auto& [key, it_order] = *it;
 
+        // ...remove it from the other indexes.
         removeOrderFromIndex(security_index, it_order);
         removeOrderFromIndex(company_index, it_order);
 
+        // ...remove it from the orders table.
         orders_table.erase(it_order);
     }
 
+    // Finally remove the key (and all its orders) from the index.
     user_index.erase(user);
 }
 
 void OrderCache::cancelOrdersForSecIdWithMinimumQty(const std::string& securityId, unsigned int minQty)
 {
+    // Get a range with all orders for the security.
     auto [range_begin, range_end] = security_index.equal_range(securityId);
 
     auto& it = range_begin;
@@ -64,17 +70,22 @@ void OrderCache::cancelOrdersForSecIdWithMinimumQty(const std::string& securityI
     {
         auto& [key, it_order] = *it;
 
+        // If order's qty is greater than minQty...
         if(it_order->qty() >= minQty)
         {
+            // ...remove the order from the other indexes.
             removeOrderFromIndex(user_index, it_order);
             removeOrderFromIndex(company_index, it_order);
 
+            // ...remove the order from the orders table.
             orders_table.erase(it_order);
 
+            // ...remove the order from the security index itself.
             it = security_index.erase(it);
         }
         else
         {
+            // The order has qty smaller than minQty. Move to the next order.
             it = std::next(it);
         }
     }
@@ -86,10 +97,11 @@ unsigned int OrderCache::getMatchingSizeForSecurity(const std::string& securityI
 
     std::vector<std::pair<OrderIterator, unsigned int>> sell_orders, buy_orders;
 
-    // Select all 'sell' and 'buy' orders for the given security
-    // and attach 'remaining qty' field to each of it (initialized by the order's qty).
+    // Get a range with all orders for the security.
     auto [range_begin, range_end] = security_index.equal_range(securityId);
 
+    // Select all 'Sell' and 'Buy' orders for the given security
+    // and attach 'remaining qty' field to each of it (initialized by the order's qty).
     for(auto it = range_begin; it != range_end; ++it)
     {
         const auto& [key, it_order] = *it;
@@ -114,7 +126,7 @@ unsigned int OrderCache::getMatchingSizeForSecurity(const std::string& securityI
             // Skip already fully matched orders.
             if(sel_it_order->company() == buy_it_order->company() || 0 == buy_remaining_qty)
             {
-                continue; // continue to the next buy order
+                continue; // Continue to the next buy order.
             }
 
             if(sell_remaining_qty > buy_remaining_qty)
@@ -126,7 +138,8 @@ unsigned int OrderCache::getMatchingSizeForSecurity(const std::string& securityI
                 sell_remaining_qty -= matched_qty;
                 buy_remaining_qty = 0;
 
-                continue; // continue to the next buy order
+                continue; // The remaining qty for the buy order has been depleted.
+                          // Continue to the next buy order.
             }
             else if(sell_remaining_qty < buy_remaining_qty)
             {
@@ -137,7 +150,8 @@ unsigned int OrderCache::getMatchingSizeForSecurity(const std::string& securityI
                 sell_remaining_qty = 0;
                 buy_remaining_qty -= matched_qty;
 
-                break; // stop processing buy orders and continue to the next sell order
+                break; // The remaining qty for the sell order has been depleted.
+                       // Stop processing the buy orders and continue to the next sell order.
             }
             else if(sell_remaining_qty == buy_remaining_qty)
             {
@@ -148,7 +162,8 @@ unsigned int OrderCache::getMatchingSizeForSecurity(const std::string& securityI
                 sell_remaining_qty = 0;
                 buy_remaining_qty = 0;
 
-                break; // stop processing buy orders and continue to the next sell order
+                break; // The remaining qty for the sell order has been depleted.
+                       // Stop processing the buy orders and continue to the next sell order.
             }
         }
     }
